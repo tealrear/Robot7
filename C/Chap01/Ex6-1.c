023e42d7d7ea4e65cf47f3b7560c62da561bc3e9
tealrear <168408736+tealrear@users.noreply.github.com>
2025-11-03 11:55:46 +0900

    Create Ex6-1.c

---
+++ C/Chap01/Ex6-1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int i = 1;
+
+	while (i<=10) 
+	{	
+		printf("i : %d\n",i);
+		i++;
+	}	//printf("i : %d\n",i++);로 가능
+
+	return 0;
+}
